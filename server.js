b7f772397f155bbad47d3a28145fb5292c113920
Galang Adi Putra Pratama Lissanto <97155903+GalangHaze@users.noreply.github.com>
2023-05-09 08:26:10 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { Storage } = require('@google-cloud/storage');
+const app = express();
+const storage = new Storage();
+
+app.get('/', async (req, res) => {
+  const bucketName = 'submission-galang-01';
+  const fileNames = ['Anime NFT.jpg', 'Jong Unch.png', 'NFT 1.png', 'NFT 2.png', 'download.png'];
+  const promises = [];
+
+  for (const fileName of fileNames) {
+    const file = storage.bucket(bucketName).file(fileName);
+    const fileExists = await file.exists();
+
+    if (fileExists[0]) {
+      promises.push(file.getSignedUrl({
+        action: 'read',
+        expires: '03-17-2025' // Tanggal kadaluarsa URL
+      }));
+    }
+  }
+
+  const urls = await Promise.all(promises);
+
+  let html = '<html><body>';
+
+  for (const url of urls) {
+    html += `<img src="${url}" /><br>`;
+  }
+
+  html += '</body></html>';
+  res.send(html);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
